1102881a1f10ca710e49fa00f971721a5cd7ccaf
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 13:18:06 -0700

    added node style box

--- src/components/gridNode.js
+++ src/components/gridNode.js
@@ -3,8 +3,15 @@ import React from 'react'
 class GridNode extends React.Component {
 
     render() {
+        const nodeStyle = {
+            height: "10px",
+            width: "10px",
+            padding: "10px",
+            margin: "0px",
+            border: "1px solid black"
+        }
         return (
-            <p>node</p>
+            <p style={nodeStyle}> {this.props.unique}</p>
         )
     }
 }
