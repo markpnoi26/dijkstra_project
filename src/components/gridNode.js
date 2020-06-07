0459e577b8030d99b631e5b01f6c404f52ea0211
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 15:23:08 -0700

    updated color change functionality

--- src/components/gridNode.js
+++ src/components/gridNode.js
@@ -6,20 +6,39 @@ class GridNode extends React.Component {
         super(props)
         this.state = {
             x: this.props.x,
-            y: this.props.y
+            y: this.props.y,
+            start: false,
+            end: false,
+            wall: false,
+            found: false,
+            from: null
         }
+    }
+
+    setColor = () => {
+        if (this.state.wall) return "black"
+        if (this.state.start) return "green"
+        if (this.state.end) return "red"
+    }
+
+    onClickHandler = () => {
+        // set conditional to render different color based on predefined selection: wall, start, end
+        console.log(`This box was clicked x=${this.state.x}, y=${this.state.y}`, this.state.wall)
+        this.setState({ wall: !this.state.wall})
     }
 
     render() {
         const nodeStyle = {
-            height: "50px",
-            width: "50px",
+            height: "25px",
+            width: "25px",
             padding: "10px",
             margin: "0px",
-            border: "1px solid black"
+            border: "1px solid black",
+            backgroundColor: this.setColor()
         }
         return (
-            <p style={nodeStyle}> x={this.state.x}, y={this.state.y}</p>
+            <div style={nodeStyle} onClick={this.onClickHandler} >
+            </div>
         )
     }
 }
