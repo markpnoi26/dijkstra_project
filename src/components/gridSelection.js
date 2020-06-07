c016b5a0e29877e1e4a21f6c1852611cb3b80ddb
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 15:30:08 -0700

    added Selections

---
+++ src/components/gridSelection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class GridSelection extends React.Component {
+    render() {
+        <div>
+            <div><p>Start</p></div>
+            <div><p>End</p></div>
+            <div><p>Wall</p></div>
+        </div>
+    }
+}
+
+export default GridSelection
