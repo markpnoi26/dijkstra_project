19cec9ba36894ae28000c668ef1520b63aa756d0
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 14:36:37 -0700

    added map functionality to grid generation

--- src/containers/gridHolder.js
+++ src/containers/gridHolder.js
@@ -4,35 +4,30 @@ import Node from '../components/gridNode'
 
 class GridHolder extends React.Component {
 
-    fillRow() {
+    fillCol(y) {
+        const colNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        return (
+            colNum.map((num) => {
+                return (<td><Node x={num} y={y} /></td>)
+            })
+        )
+    }
+
+    fillRow(y) {
         return (
             <tr> 
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
-                <td> <Node /></td>
+               {this.fillCol(y)}
             </tr>
         )
     }
 
     render() {
+        const rowNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         return (
             <table>
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
-                {this.fillRow()}
+                {rowNum.map((num) => {
+                    return this.fillRow(num)
+                })}
             </table>
         )
     }
