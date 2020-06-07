e3f2c4f9732c9f2300424c1337606e3584125a21
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 14:36:49 -0700

    added x&y props

--- src/components/gridNode.js
+++ src/components/gridNode.js
@@ -2,16 +2,24 @@ import React from 'react'
 
 class GridNode extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            x: this.props.x,
+            y: this.props.y
+        }
+    }
+
     render() {
         const nodeStyle = {
-            height: "10px",
-            width: "10px",
+            height: "50px",
+            width: "50px",
             padding: "10px",
             margin: "0px",
             border: "1px solid black"
         }
         return (
-            <p style={nodeStyle}> {this.props.unique}</p>
+            <p style={nodeStyle}> x={this.state.x}, y={this.state.y}</p>
         )
     }
 }
