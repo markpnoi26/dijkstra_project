fb6bab359471b2d6baa6d8e739f7d9b632cb371b
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 15:25:09 -0700

    changed gridNode name & refactored to incorporate key

--- src/containers/gridHolder.js
+++ src/containers/gridHolder.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import Node from '../components/gridNode'
+import GridNode from '../components/gridNode'
 
 
 class GridHolder extends React.Component {
 
     fillCol(y) {
         const colNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        return (
-            colNum.map((num) => {
-                return (<td><Node x={num} y={y} /></td>)
+        return(
+            colNum.map((x) => {
+                return (
+                    <td key={`x${x}`}>
+                        <GridNode x={x} y={y} />
+                    </td>
+                )
             })
         )
     }
 
     fillRow(y) {
-        return (
-            <tr> 
+        return(
+            <tr key={`y${y}`}> 
                {this.fillCol(y)}
             </tr>
         )
@@ -23,11 +27,13 @@ class GridHolder extends React.Component {
 
     render() {
         const rowNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        return (
+        return(
             <table>
-                {rowNum.map((num) => {
-                    return this.fillRow(num)
-                })}
+                <tbody>
+                    {rowNum.map((y) => {
+                        return this.fillRow(y)
+                    })}
+                </tbody>
             </table>
         )
     }
