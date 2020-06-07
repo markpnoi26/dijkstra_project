5f2157cab142be114619494aa3cbf6283883a3e0
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-07 12:57:50 -0700

    changed the contents of App & Index

---
+++ src/components/gridNode.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class GridNode extends React.Component {
+
+    render() {
+        return (
+            <p>I am a node</p>
+        )
+    }
+}
+
+export default GridNode
---
+++ src/containers/gridHolder.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Node from '../components/gridNode'
+
+
+class GridHolder extends React.Component {
+
+    fillRow() {
+
+    }
+
+    fillCol() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> This is the Grid Holder </h1>
+                <Node />
+            </div>
+        )
+    }
+}
+
+export default GridHolder
