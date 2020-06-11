c2306742a8770794535dd7ffc0b4abe2977f44f6
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-11 12:33:06 -0700

    added fully functional reset button.

--- src/containers/gridHolder.js
+++ src/containers/gridHolder.js
@@ -11,13 +11,12 @@ export default class GridHolder extends React.Component {
             isBuilding: false,
             isTearing: false,
             isCurrentlyAnimating: false,
-            originalStart: [5, 5],
-            originalEnd: [8, 5],
-            start: [],
-            end: [],
-            size: 20,
-            originalGrid: [],
-            modifiedGrid: []
+            originalStart: [15, 5],
+            originalEnd: [15, 24],
+            start: [15, 5],
+            end: [15, 24],
+            size: 30,
+            grid: []
         }
     }
 
@@ -45,54 +44,49 @@ export default class GridHolder extends React.Component {
     // updates the wall 
 
     addWall = (row, col) => {
-        const grid = this.state.modifiedGrid.slice()
+        const grid = this.state.grid.slice()
         const [startRow, startCol] = this.state.start
         const [endRow, endCol] = this.state.end
         if (row === startRow && col === startCol) return 
         if (row === endRow && col === endCol) return 
         grid[row][col].wall = true
         this.setState({
-            modifiedGrid: grid
+            grid: grid
         })
     }
 
     deleteWall = (row, col) => {
-        const grid = this.state.modifiedGrid.slice()
+        const grid = this.state.grid.slice()
         const [startRow, startCol] = this.state.start
         const [endRow, endCol] = this.state.end
         if (row === startRow && col === startCol) return 
         if (row === endRow && col === endCol) return 
         grid[row][col].wall = false
         this.setState({
-            modifiedGrid: grid
+            grid: grid
         })
     }
 
     // updates the start point & end point
     updateStart = (row, col) => {
-        const grid = this.state.modifiedGrid
+        const grid = this.state.grid.slice()
         const [startRow, startCol] = this.state.start
-        if (startRow && startCol) {
-            grid[startRow][startCol].start = false
-        }
+        grid[startRow][startCol].start = false
         grid[row][col].start = true
         this.setState({
             start: [row, col],
-            modifiedGrid: grid
+            grid: grid
         })
     }
 
     updateEnd = (row, col) => {
-        const grid = this.state.modifiedGrid
+        const grid = this.state.grid.slice()
         const [endRow, endCol] = this.state.end
-
-        if (endRow && endCol) {
-            grid[endRow][endCol].end = false
-        }
+        grid[endRow][endCol].end = false
         grid[row][col].end = true
         this.setState({
             end: [row, col],
-            modifiedGrid: grid
+            grid: grid
         })
     }
 
@@ -101,7 +95,7 @@ export default class GridHolder extends React.Component {
         // if the input is not valid, the code will not execute.
         if (!this.state.start.length || !this.state.end.length) return alert("Please pick a start point and an end point.")
         // declare constants first
-        const [path, visited] = dijkstra(this.state.modifiedGrid, this.state.start, this.state.end)
+        const [path, visited] = dijkstra(this.state.grid, this.state.start, this.state.end)
         const pathLen = path.length
         const visitedNodesLen = visited.length
 
@@ -115,24 +109,24 @@ export default class GridHolder extends React.Component {
 
         //animate visited nodes first followed by path (set on a timer after visited nodes are done animating)
         setTimeout(() => {
-            const grid = this.state.modifiedGrid
+            const grid = this.state.grid
             for (let i = 0; i < path.length; i++) {
                 setTimeout(() => {
                     const node = path[i];
                     grid[node[0]][node[1]].visited = false
                     grid[node[0]][node[1]].path = true
-                    this.setState({ modifiedGrid: grid })
+                    this.setState({ grid: grid })
                 }, this.props.animationSpeed * i)
             }
         }, visitedNodesLen * this.props.animationSpeed + 500)
 
 
-        const grid = this.state.modifiedGrid
+        const grid = this.state.grid
         for (let i = 0; i < visitedNodesLen; i++) {
             setTimeout(() => {
                 const node = visited[i];
                 grid[node[0]][node[1]].visited = true
-                this.setState({ modifiedGrid: grid })
+                this.setState({ grid: grid })
             }, this.props.animationSpeed * i)
         }
     }
@@ -153,9 +147,10 @@ export default class GridHolder extends React.Component {
     resetBoard = () => {
         const newGrid = this.generateGrid() 
         this.setState({
-            modifiedGrid: newGrid,
-            start: [],
-            end: []
+            grid: newGrid
+        }, () => {
+            this.updateStart(this.state.originalStart[0], this.state.originalStart[1])
+            this.updateEnd(this.state.originalEnd[0], this.state.originalEnd[1])
         })
     }
 
@@ -163,7 +158,7 @@ export default class GridHolder extends React.Component {
     fillCol(rowIdx) {
 
         return(
-            this.state.modifiedGrid[rowIdx].map((node, colIdx) => {
+            this.state.grid[rowIdx].map((node, colIdx) => {
                 return (
                     <GridNode 
                         key={`node-row-${rowIdx}-col-${colIdx}`}
@@ -200,7 +195,10 @@ export default class GridHolder extends React.Component {
         // generate grid
         const generatedGrid = this.generateGrid()
         this.setState({
-            modifiedGrid: generatedGrid
+            grid: generatedGrid
+        }, () => {
+            this.updateStart(this.state.start[0], this.state.start[1])
+            this.updateEnd(this.state.end[0], this.state.end[1])
         })
     }
 
@@ -208,7 +206,7 @@ export default class GridHolder extends React.Component {
         return(
             <> 
                 <div className="main-holder" style={{width: "1000px", height: "800px"}} >
-                    {this.state.modifiedGrid.map((row, rowIdx) => {
+                    {this.state.grid.map((row, rowIdx) => {
                         return this.fillRow(rowIdx)
                     })}
                 </div>
