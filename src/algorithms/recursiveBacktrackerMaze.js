840de77e4c6d66e8537a6f6e8530d9d63eafa7bf
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-15 21:15:39 -0700

    added a functional recursive Backtrack maze generator

--- src/algorithms/recursiveBacktrackerMaze.js
+++ src/algorithms/recursiveBacktrackerMaze.js
@@ -1,3 +1,88 @@
-const recursiveBacktrack = (board) => {
-    
+const recursiveBacktrack = (board, start=[1, 1]) => {
+    const colMax = board[0].length
+    const rowMax = board.length
+    const visited = new Array(board.length)
+    const queue = new Stack()
+
+    // return values
+    const nodesVisited = []
+
+    let deQ;
+
+    for (let row = 0; row < board.length; row++) {
+        visited[row] = new Array(board[0].length).fill(false)
+    }
+    visited[start[0]][start[1]] = true
+
+    queue.enqueue(start, [])
+
+    while (queue.values.length) {
+        deQ = queue.dequeue()
+        if (deQ) {
+            // check all directions, and add to priority queue
+            const directions = shuffleDirections([[0, 2], [2, 0], [-2, 0], [0, -2]])
+            const curRow = deQ.node[0]
+            const curCol = deQ.node[1]
+
+            nodesVisited.push([curRow, curCol])
+
+            for (let direction of directions) {
+                let dRow = curRow + direction[0]
+                let dCol = curCol + direction[1]
+                // check if its within bounds
+                if (dRow >= 0 && dCol >= 0 && dRow < rowMax && dCol < colMax && !visited[dRow][dCol]) {
+                    visited[dRow][dCol] = true
+                    // console.log((curRow + dRow) / 2)
+                    // console.log(curCol + dCol) / 2)
+                    nodesVisited.push([(curRow+dRow)/2, (curCol+dCol)/2])
+                    queue.enqueue([dRow, dCol], [curRow, curCol])
+                }
+            }
+        }
+
+    }
+
+    console.log(nodesVisited)
+    return nodesVisited
 }
+
+const shuffleDirections = (directions) => {
+    let currentIdx = directions.length, tempVal, randomIdx
+
+    while (0 !== currentIdx) {
+        randomIdx = Math.floor(Math.random() * currentIdx)
+        currentIdx--
+
+        tempVal = directions[currentIdx]
+        directions[currentIdx] = directions[randomIdx]
+        directions[randomIdx] = tempVal
+    }
+
+    return directions
+
+}
+
+const getRandomStart = (nodesVisited) => {
+} 
+
+const getRandomEnd = (nodesVisited) => {
+
+}
+
+class Stack {
+    constructor() {
+        this.values = []
+    }
+
+    enqueue = (node, from) => {
+        this.values.push({ node, from })
+    }
+
+    dequeue = () => {
+        return this.values.pop()
+    }
+
+
+}
+
+export default recursiveBacktrack
