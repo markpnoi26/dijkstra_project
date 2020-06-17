2f9c84d54dab86ec654d4f9a36dd296d1782f162
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-16 21:31:21 -0700

    Added functional legend

--- src/containers/legendContainer.js
+++ src/containers/legendContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Node from '../components/gridNode'
 
 export default class LegendContainer extends React.Component {
     constructor(props) {
@@ -11,7 +12,18 @@ export default class LegendContainer extends React.Component {
     render () {
         return(
             <div>
-                This is the legend.
+                <h1> Legend </h1>
+                Start Node: <Node start={true} />
+                End Node: <Node end={true}/>
+                Visited Node: <Node  weight={1} visited={true} start={false} end={false}/>
+                Shortest Path Node: <Node  weight={1} path={true}/>
+                Shortest Resisted Node (3): <Node weight={3} path={true}/>
+                Shortest Resisted Node (5): <Node weight={5} path={true}/>
+                Wall: <Node wall={true}/>
+                Node with Resistance (3) <Node weight={3}/>
+                Node with Resistance (5) <Node weight={5}/>
+                Open Node/Unvisited Node <Node />
+
             </div>
         )
     }
