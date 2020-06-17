80ea5f54a22b2ac4348d2d44dd07bfb09de9aa6f
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-16 21:12:15 -0700

    created a new component

---
+++ src/containers/legendContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class LegendContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return(
+            <div>
+                This is the legend.
+            </div>
+        )
+    }
+}
