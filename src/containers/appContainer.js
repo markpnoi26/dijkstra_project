27d1ed8c3783285006d41212b05b4f0dc10dab15
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-17 17:41:43 -0700

    uncommented modal container, not yet functional

--- src/containers/appContainer.js
+++ src/containers/appContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import GridContainer from './gridContainer'
 import LegendContainer from './legendContainer'
+// import TutorialContainer from './tutorialModalContainer'
 import '../component-styles/app.css'
 
 
@@ -26,6 +27,7 @@ class AppContainer extends React.Component {
     render() {
         return (
             <div className="app-wrapper">
+                {/* <TutorialContainer /> */}
                 <GridContainer 
                     animationSpeed={this.state.animationSpeed}
                     colSize={this.state.colSize}
