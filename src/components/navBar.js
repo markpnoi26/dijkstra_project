8a1f2202bc4c076b5629baaba5a3dd78a28d5cf6
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-18 13:23:59 -0700

    added more css style and minor text updates

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -8,34 +8,34 @@ export default class NavBar extends React.Component {
         return(
             <div className="nav-bar">
                 <div>
-                    <label className="label-name"> Manipulate grid: </label>
+                    <label className="label-name"> Manipulate Grid: </label>
                     <select className="drop-down" value={this.props.currentSelection} onChange={this.props.handleSelectionChange} disabled={this.props.isCurrentlyAnimating}> 
                         <option value="buildWall">Build Node Walls</option>
                         <option value="tearWall">Delete Node Walls</option>
                         <option value="toggleWeight">Toggle Node Resistance</option>
                     </select>
 
-                    <label className="label-name"> Pathfinding Algorithm: </label>
+                    <label className="label-name"> Algorithm: </label>
                     <select className="drop-down" value={this.props.currentAlgorithm} onChange={this.props.handleAlgorithmChange} disabled={this.props.isCurrentlyAnimating}> 
-                        <option value="dfs">DFS</option>
-                        <option value="bfs">BFS</option>
                         <option value="dijkstra">Dijkstra Shortest Path</option>
                         <option value="aStar">A*</option>
+                        <option value="dfs">DFS</option>
+                        <option value="bfs">BFS</option>
                     </select>
-                    <label className="label-name"> Exploration Mode: </label>
+                    <label className="label-name"> Mode: </label>
                     <select className="drop-down" value={this.props.currentMode} onChange={this.props.handleModeChange} disabled={this.props.isCurrentlyAnimating}> 
-                        <option value="rook">Rook (horizonal/vertical exploration)</option>
-                        <option value="bishop">Bishop (diagonal exploration)</option>
-                        <option value="queen">Queen (all directions)</option>
+                        <option value="rook">Rook</option>
+                        <option value="bishop">Bishop</option>
+                        <option value="queen">Queen</option>
                     </select>
-                    <button className="button" onClick={this.props.drawVisualization} disabled={this.props.isCurrentlyAnimating}> Visualize Custom Path Finding</button>
+                    <button className="button-visualize" onClick={this.props.drawVisualization} disabled={this.props.isCurrentlyAnimating}> Visualize Path Finding</button>
                 </div>
 
                 <div>
                     <button className="button" onClick={this.props.resetBoard} disabled={this.props.isCurrentlyAnimating}>Reset Board</button>
                     <button className="button" onClick={this.props.resetWalls} disabled={this.props.isCurrentlyAnimating}>Reset Walls</button>
                     <button className="button" onClick={this.props.resetVisitedPath} disabled={this.props.isCurrentlyAnimating}>Reset Visited/Path</button>
-                    <button className="button" onClick={this.props.drawMaze} disabled={this.props.isCurrentlyAnimating}> Create Random Maze</button>
+                    <button className="button" onClick={this.props.drawMaze} disabled={this.props.isCurrentlyAnimating}> Generate Random Maze </button>
                 </div>
 
             </div>
