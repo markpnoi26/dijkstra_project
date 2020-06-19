f75f7137abacf03ae1dcfab6cc8ad2ac2e02f36c
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-06-18 18:33:55 -0700

    added animation speed changing

--- src/components/settings.js
+++ src/components/settings.js
@@ -4,14 +4,23 @@ export default class Settings extends React.Component {
     constructor(props) {
         super(props)
         this.state ={
-
+            animationSpeed: this.props.animationSpeed
         }
+    }
+
+    handleSpeedChange = (event) => {
+        this.setState({
+            animationSpeed: event.target.value
+        }, () => {
+            this.props.updateAnimationSpeed(this.state.animationSpeed)
+        })
     }
 
     render() {
         return (
             <div>
                 Settings!!
+                <input value={this.state.animationSpeed} onChange={this.handleSpeedChange} />
                 <button onClick={this.props.closeSettingsModal}> close </button>
             </div>
         )
